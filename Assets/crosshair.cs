bcaf2e920a600ae805b0df5081fd389294972b09
soeiner <sven.langner@tu-braunschweig.de>
2016-06-16 01:50:16 +0200

    third person steuerung

--- Assets/crosshair.cs
+++ Assets/crosshair.cs
@@ -7,13 +7,18 @@ public class crosshair : MonoBehaviour {
 	void Start () {
         Cursor.visible = false;
     }
+
+	public float x = 0;
+	public float y = 0;
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 mouse = Input.mousePosition;
-        mouse = Camera.main.ScreenToWorldPoint(mouse);
-        mouse.z = 0;
-        transform.position = mouse;
+		Vector3 position = Camera.main.ScreenToWorldPoint(mouse);
+		x = mouse.x;
+		y = mouse.y;
+        position.z = 0;
+        transform.position = position;
         if (Input.GetKey(KeyCode.Escape)) { Cursor.visible = true; }
 
 
---
+++ Assets/player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+    
+    // Use this for initialization
+    public float speed = 1;
+
+	private GameObject pistolStrikeObject;
+	public Transform pistolStrike;
+
+    void Start () {
+		transform.rotation = Quaternion.Euler (0, 0, 0);
+		//pistolStrikeObject = new 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 player = transform.position;
+		Vector3 difference = mouse - player;
+		//transform.rotation = Quaternion.Euler (0, 0, (Mathf.Atan2 (difference.y, difference.x) * 180 / Mathf.PI) % 360);
+		Vector3 forward = transform.rotation * Vector3.up;
+		Vector3 left = transform.rotation * Vector3.left;
+		Vector3 backward = transform.rotation * Vector3.down;
+		Vector3 right = transform.rotation * Vector3.right;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)){
+            if (Input.GetKey(KeyCode.W)) { transform.position += forward * Time.deltaTime * speed; }
+            if (Input.GetKey(KeyCode.A)) { transform.position += left * Time.deltaTime * speed; }
+            if (Input.GetKey(KeyCode.S)) { transform.position += backward * Time.deltaTime * speed; }
+            if (Input.GetKey(KeyCode.D)) { transform.position += right * Time.deltaTime * speed; }
+        }
+		if (Input.GetMouseButton(0)) {
+			pistolStrike.rotation = transform.rotation;
+			pistolStrike.position = transform.position + transform.rotation * Vector3.up * 10;
+		}
+    }
+}
